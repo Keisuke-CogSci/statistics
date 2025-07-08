df371efc06e9e1fade5ca7fcc270278d28925658
KSK-UT <lesucre326@gmail.com>
2025-07-08 21:32:18 +0900

    Quartz sync: Jul 8, 2025, 9:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Obsidianによる資料置き場",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
